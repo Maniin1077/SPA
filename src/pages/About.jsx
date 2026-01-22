1260d4628e4d4356b580bcd614e53c3a1c9a7eef
Baratam Sri Manikanta <bsmanikanta2004@gmail.com>
2026-01-22 23:10:26 +0530

    Initial commit for SPA

---
+++ src/pages/About.jsx
@@ -0,0 +1,17 @@
+function About() {
+  return (
+    <div style={styles.container}>
+      <h1>About</h1>
+      <p>Hi, I am Manikanta, UID: 23BAI70506. This is a Single Page Application by using React.</p>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    padding: "40px",
+    textAlign: "center"
+  }
+};
+
+export default About;
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,18 @@
+function Contact() {
+  return (
+    <div style={styles.container}>
+      <h1>Contact</h1>
+      <p>Email: bsmanikanta@gmail.com</p>
+      <p>LinkedIn: www.linkedin.com/in/baratam-sri-manikanta-083117295</p>
+    </div>
+  );
+}
+
+const styles = {
+  container: {
+    padding: "40px",
+    textAlign: "center"
+  }
+};
+
+export default Contact;
